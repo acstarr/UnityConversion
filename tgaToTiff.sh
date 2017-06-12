aa1f678c4fc9819e9080d5b15ab525a8425e5bc0
Adam Starr <acstarr14@gmail.com>
2017-06-12 16:19:43 -0500

    added arguments

--- tgaToTiff.sh
+++ tgaToTiff.sh
@@ -1,16 +1,11 @@
-# recursivly search folders from file given
-# if find "path/XX.tga" convert to "path/XX.tiff" with ImageMagick
-#find . -type d -exec ./tgaToTiff.sh
+#!/bin/bash
+# go through inputs given and convert .tga to .tiff
 
-#for folder in directory list contents
-for elt in $(ls)
+for elt in "$@"
 do
 	if [[ $elt == *.tga ]]
+	then
 		filename=${elt%.tga}
 		echo $filename #magick convert elt ${filename%.tiff}
 	fi
 done
-
-#for element that ends in .tga
-
-#call script on sub folders
