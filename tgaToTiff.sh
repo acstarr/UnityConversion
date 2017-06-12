d14f252e08bb58c8b90eafc299d88b8f2bb3829d
Adam Starr <acstarr14@gmail.com>
2017-06-12 16:48:40 -0500

    change structure

--- tgaToTiff.sh
+++ tgaToTiff.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # go through inputs given and convert .tga to .tiff
 
-for elt in "$@"
-do
-	if [[ $elt == *.tga ]]
-	then
-		filename=${elt%.tga}
-		echo $filename #magick convert elt ${filename%.tiff}
-	fi
+for x in *.tga; do 
+  t=${x%.tga}.tiff
+  echo "$x" 
+  echo "$t"
+  #magick convert elt ${filename%.tiff} && echo "converted $x -> $t"
 done
