c3348d45be40495d76af1426c8b59098926e3165
Adam Starr <acstarr14@gmail.com>
2017-06-13 16:20:23 -0500

    add support for .mat files and check for null globs

--- tgaToTiff.sh
+++ tgaToTiff.sh
@@ -4,8 +4,17 @@
 #find . -type d -execdir sh /mnt/c/Users/acs9wd/Desktop/iSocial/tgaToTiff.sh \;
 
 for x in *.tga; do 
-  t=${x%.tga}.tiff
-  #echo "$x" 
-  #echo "$t"
-  magick convert elt ${filename%.tiff} && echo "converted $x -> $t"
+	if [ "${#x}" -gt "5" ]
+  		then
+  		t=${x%.tga}.tiff
+ 		convert ${x} ${t} && echo "converted $x -> $t"
+ 	fi
+done
+
+for x in *.mat; do 
+	if [ "${#x}" -gt "5" ]
+  		then
+  		t=${x%.mat}.tiff
+ 		convert ${x} ${t} && echo "converted $x -> $t"
+ 	fi
 done
