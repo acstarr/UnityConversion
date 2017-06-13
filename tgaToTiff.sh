d4913570fc8cf013fea7477a2cd9a269ffc226d9
Adam Starr <acstarr14@gmail.com>
2017-06-13 14:18:09 -0500

    add comment with how to call

--- tgaToTiff.sh
+++ tgaToTiff.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Adam Starr 2017-06-13
 # go through inputs given and convert .tga to .tiff
+#find . -type d -execdir sh /mnt/c/Users/acs9wd/Desktop/iSocial/tgaToTiff.sh \;
 
 for x in *.tga; do 
   t=${x%.tga}.tiff
