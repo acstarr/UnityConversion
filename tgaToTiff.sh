61ce0310491ae0f431af241b21544d3f7c6adf56
Adam Starr <acstarr14@gmail.com>
2017-06-13 14:14:35 -0500

    Uncomment echos

--- tgaToTiff.sh
+++ tgaToTiff.sh
@@ -3,7 +3,7 @@
 
 for x in *.tga; do 
   t=${x%.tga}.tiff
-  echo "$x" 
-  echo "$t"
-  #magick convert elt ${filename%.tiff} && echo "converted $x -> $t"
+  #echo "$x" 
+  #echo "$t"
+  magick convert elt ${filename%.tiff} && echo "converted $x -> $t"
 done
